240f341918aed165627baec6ffaaf749fc8c2c38
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-11-10 19:17:57 -0500

    simple header and footer

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -1,8 +1,14 @@
 <template>
-  <div>
-    Navbar
-    <button class="waves-effect waves-light btn">Test</button>  
-  </div>
+  <nav>
+    <div class="nav-wrapper orange accent-4">
+      <a href="#" class="brand-logo">Arbeiter</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="/">Login</a></li>
+        <li><a href="/register">Registrarme</a></li>
+      </ul>
+    </div>
+  </nav>
+        
 </template>
 
 <script>
