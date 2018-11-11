c8f088691b69f809787e7752b748f36518845206
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2018-11-11 12:33:57 -0500

    add brand logo

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <div class="nav-wrapper indigo">
-      <a href="/" class="brand-logo">Arbeiter</a>
+      <a href="/" class="brand-logo">
+        <img :src="baseLogo">
+        <span class="brand-name">Arbeiter</span>
+      </a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="/">Login</a></li>
         <li><a href="/registro">Registrarme</a></li>
@@ -13,10 +16,28 @@
   </nav>
         
 </template>
-
 <script>
+import Logo from '../../assets/arbeiter-logo.png';
+
 export default {
-  
+  data() {
+    return {
+      baseLogo: Logo,
+    }
+  }  
 }
 </script>
+
+<style scoped>
+img {
+  height: 65px !important;
+  width: auto;
+  opacity: 0.9;
+}
+
+.brand-name {
+  margin-left: 20px;
+  position: fixed;
+}
+</style>
 
