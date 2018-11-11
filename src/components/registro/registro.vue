0d0538a4d94a65c7845406db832b3307f4d7d695
Adrian Bautista <adrianbautistaorozco@gmail.com>
2018-11-10 21:04:56 -0500

    Feed 03 catalogo

--- src/components/registro/registro.vue
+++ src/components/registro/registro.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>Registro</div>
+  <div class="d-flex justify-content-center grey lighten-5">
+    <div class="container">
+      <div class="row">
+    <div class="card col s12">
+      <h1>Registro</h1>
+      <div class="row">
+        <div class="input-field col s12 m12">
+          <input id="first_name" type="text" class="validate">
+          <label for="first_name">Nombre Completo</label>
+        </div>
+        
+      </div>
+      
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12 m12">
+          <input id="address" type="text" class="validate">
+          <label for="address">Direccion</label>
+        </div>
+        
+      </div>
+
+      <div class="row">
+        <div class="input-field col s12">
+          <a href="/dashboard" class="waves-effect waves-light btn orange accent-4">Registrar</a>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,3 +45,12 @@ export default {
 }
 </script>
 
+<style scoped>
+  .d-flex{
+    display: flex;
+    justify-content: center;
+  }
+  .row{
+    margin-bottom: 0;
+  }
+</style>
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 
 import Registro from './components/registro/registro.vue';
 import Login from './components/login/login.vue';
+import Dashboard from './components/dashboard/dashboard.vue';
+import Catalogo from './components/catalogo/catalogo.vue';
 
 Vue.use(Router);
 
@@ -18,6 +20,16 @@ export default new Router({
       name: 'registro',
       path: '/register',
       component: Registro,
+    },
+    {
+      name: 'dashboard',
+      path: '/dashboard',
+      component: Dashboard,
+    },
+    {
+      name: 'catalogo',
+      path: '/catalogo',
+      component: Catalogo,
     },
   ]
 })
